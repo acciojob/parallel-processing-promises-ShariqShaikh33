0fcfcbcacdd2dea369b522232dc8c3e433ce514a
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-11-30 19:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,45 +9,44 @@ const images = [
 ];
 
 function downloadimg(url){
-	return new Promise((resolve, reject) => {
-		fetch(url)
-			.then(response => {
-				if (!response.ok){
-					throw new Error('HTTP error! status: ${response.status}');
-				}
-				return response.blob();
-			})
-			.then(blob => {
-				let img = new Image();
-				img.src = URL.createObjectURL(blob);
-				resolve(img);
-			})
-			.catch(e => {
-				reject(new Error("Failed to load image's URL: ${url}"));
-			});
-	});
-	
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => {
+                if (!response.ok){
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.blob();
+            })
+            .then(blob => {
+                let imgURL = URL.createObjectURL(blob);
+                resolve(imgURL);
+            })
+            .catch(e => {
+                reject(new Error(`Failed to load image's URL: ${url}`));
+            });
+    });
 }
 
 function loadimg(images){
-	let imagePromise = images.map(image => {
-		return downloadimg(image.url);
-	});
-	return imagePromise;
+    let imagePromise = images.map(image => {
+        return downloadimg(image.url); // return the promise
+    });
+    return imagePromise; // return the array of promises
 }
 
-btn.addEventListener('click',function(){
-	let imagepromises = loadimg(images);
-	Promise.all(imagepromises)
-	.then(urls => {
-		
-		urls.forEach((url) => {
-			let imageElement = document.createElement('img');
-			imageElement.src = url;
-			output.appendChild(imageElement);
-		});
-	}).catch(err => {
-		console.error(err);
-	});
+// Load images when button is clicked
+btn.addEventListener('click', function() {
+    let imagePromises = loadimg(images);
+    Promise.all(imagePromises)
+        .then(urls => {
+            urls.forEach(url => {
+				console.log(url);
+                let imageElement = document.createElement('img');
+                imageElement.src = url;
+                output.appendChild(imageElement);
+            });
+        }).catch(err => {
+            console.error(err);
+        });
 });
 
